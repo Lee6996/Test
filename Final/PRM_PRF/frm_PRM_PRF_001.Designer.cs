2735cb0028a88a5cbd381d73f9ff4c954b4aa62e
민주 <mj09287@gmail.com>
2021-01-26 21:55:51 +0900

    민주 frm_PRM_PRF_001 추가

---
+++ Final/PRM_PRF/frm_PRM_PRF_001.Designer.cs
@@ -0,0 +1,185 @@
+﻿
+namespace Final.PRM_PRF
+{
+    partial class frm_PRM_PRF_001
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.dgvPRM_PRF_001 = new System.Windows.Forms.DataGridView();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPRM_PRF_001)).BeginInit();
+            this.panel6.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.button2);
+            this.panel4.Controls.Add(this.button1);
+            this.panel4.Controls.SetChildIndex(this.label2, 0);
+            this.panel4.Controls.SetChildIndex(this.label3, 0);
+            this.panel4.Controls.SetChildIndex(this.dateTimePicker1, 0);
+            this.panel4.Controls.SetChildIndex(this.dateTimePicker2, 0);
+            this.panel4.Controls.SetChildIndex(this.label4, 0);
+            this.panel4.Controls.SetChildIndex(this.textBox1, 0);
+            this.panel4.Controls.SetChildIndex(this.button22, 0);
+            this.panel4.Controls.SetChildIndex(this.button23, 0);
+            this.panel4.Controls.SetChildIndex(this.label16, 0);
+            this.panel4.Controls.SetChildIndex(this.textBox6, 0);
+            this.panel4.Controls.SetChildIndex(this.button24, 0);
+            this.panel4.Controls.SetChildIndex(this.button1, 0);
+            this.panel4.Controls.SetChildIndex(this.button2, 0);
+            // 
+            // label2
+            // 
+            this.label2.Size = new System.Drawing.Size(149, 20);
+            this.label2.Text = "조회조건 : 작업 지시 일자";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1052, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "실적보정";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1154, 21);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "실적분할";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.panel5.Controls.Add(this.dgvPRM_PRF_001);
+            this.panel5.Controls.Add(this.panel6);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel5.Location = new System.Drawing.Point(0, 67);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(1266, 739);
+            this.panel5.TabIndex = 20;
+            // 
+            // dgvPRM_PRF_001
+            // 
+            this.dgvPRM_PRF_001.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvPRM_PRF_001.BackgroundColor = System.Drawing.SystemColors.HighlightText;
+            this.dgvPRM_PRF_001.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPRM_PRF_001.Location = new System.Drawing.Point(0, 45);
+            this.dgvPRM_PRF_001.Name = "dgvPRM_PRF_001";
+            this.dgvPRM_PRF_001.RowTemplate.Height = 23;
+            this.dgvPRM_PRF_001.Size = new System.Drawing.Size(1248, 682);
+            this.dgvPRM_PRF_001.TabIndex = 1;
+            this.dgvPRM_PRF_001.Tag = "";
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.SystemColors.Control;
+            this.panel6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel6.Controls.Add(this.panel7);
+            this.panel6.Controls.Add(this.label5);
+            this.panel6.Location = new System.Drawing.Point(6, 13);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(194, 26);
+            this.panel6.TabIndex = 0;
+            // 
+            // panel7
+            // 
+            this.panel7.Controls.Add(this.label6);
+            this.panel7.Location = new System.Drawing.Point(-572, -229);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(194, 26);
+            this.panel7.TabIndex = 2;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(34, 6);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 12);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "생산의뢰";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label5.Location = new System.Drawing.Point(34, 6);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 15);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "조회내역";
+            // 
+            // frm_PRM_PRF_001
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.ClientSize = new System.Drawing.Size(1266, 828);
+            this.Controls.Add(this.panel5);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.Name = "frm_PRM_PRF_001";
+            this.Load += new System.EventHandler(this.frm_PRM_PRF_001_Load);
+            this.Controls.SetChildIndex(this.panel4, 0);
+            this.Controls.SetChildIndex(this.panel5, 0);
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPRM_PRF_001)).EndInit();
+            this.panel6.ResumeLayout(false);
+            this.panel6.PerformLayout();
+            this.panel7.ResumeLayout(false);
+            this.panel7.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.DataGridView dgvPRM_PRF_001;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ Final/PRM_PRF/frm_PRM_PRF_001.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Final.PRM_PRF
+{
+    public partial class frm_PRM_PRF_001 : Final.KPI_RPT.KPI_RPT_MDIParent
+    {
+        public frm_PRM_PRF_001()
+        {
+            InitializeComponent();
+        }
+
+        private void frm_PRM_PRF_001_Load(object sender, EventArgs e)
+        {
+
+            CommonUtil.SetInitGridView(dgvPRM_PRF_001);
+            CommonUtil.AddGridTextColumn(dgvPRM_PRF_001, "작업지시번호", "CommonCode", 150);
+            CommonUtil.AddGridTextColumn(dgvPRM_PRF_001, "작업지시일자", "CommonCode", 150);
+            CommonUtil.AddGridTextColumn(dgvPRM_PRF_001, "작업지시상태", "name", 150);
+            CommonUtil.AddGridTextColumn(dgvPRM_PRF_001, "품목코드", "safestock", 150);
+            CommonUtil.AddGridTextColumn(dgvPRM_PRF_001, "품목명", "stock", 200);
+            CommonUtil.AddGridTextColumn(dgvPRM_PRF_001, "작업장", "safestock", 150);
+            CommonUtil.AddGridTextColumn(dgvPRM_PRF_001, "투입수량", "stock", 150);
+            CommonUtil.AddGridTextColumn(dgvPRM_PRF_001, "산출수량", "safestock", 150);
+            CommonUtil.AddGridTextColumn(dgvPRM_PRF_001, "생산수량", "stock", 150);
+        }
+    }
+}
