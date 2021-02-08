2a7fc144c62c3a2be18f46c01cd2aa65df7244c5
GSungJae <71108073+GSungJae@users.noreply.github.com>
2021-02-08 08:30:11 +0800

    0208

--- Final/KPI_RPT/frm_KPI_RPT_P.Designer.cs
+++ Final/KPI_RPT/frm_KPI_RPT_P.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
-namespace Final
+namespace Final.KPI_RPT
 {
-    partial class frm_KPI_RPT_1
+    partial class frm_KPI_RPT_P
     {
         /// <summary>
         /// Required designer variable.
@@ -29,43 +29,44 @@ namespace Final
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.dgv_Process = new System.Windows.Forms.DataGridView();
             this.label2 = new System.Windows.Forms.Label();
+            this.dgv_Process = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgv_Process)).BeginInit();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("굴림", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label1.Location = new System.Drawing.Point(12, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(89, 19);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "공정목록";
-            // 
-            // dgv_Process
-            // 
-            this.dgv_Process.BackgroundColor = System.Drawing.SystemColors.HighlightText;
-            this.dgv_Process.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgv_Process.Location = new System.Drawing.Point(12, 45);
-            this.dgv_Process.Name = "dgv_Process";
-            this.dgv_Process.RowTemplate.Height = 23;
-            this.dgv_Process.Size = new System.Drawing.Size(480, 304);
-            this.dgv_Process.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("굴림", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label2.Location = new System.Drawing.Point(143, 95);
+            this.label2.Location = new System.Drawing.Point(142, 96);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(193, 19);
-            this.label2.TabIndex = 2;
+            this.label2.TabIndex = 5;
             this.label2.Text = "공정코드 - 공정이름";
             // 
-            // frm_KPI_RPT_1
+            // dgv_Process
+            // 
+            this.dgv_Process.BackgroundColor = System.Drawing.SystemColors.HighlightText;
+            this.dgv_Process.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_Process.Location = new System.Drawing.Point(11, 46);
+            this.dgv_Process.Name = "dgv_Process";
+            this.dgv_Process.RowTemplate.Height = 23;
+            this.dgv_Process.Size = new System.Drawing.Size(480, 304);
+            this.dgv_Process.TabIndex = 4;
+            this.dgv_Process.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_Process_CellDoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(11, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "공정목록";
+            // 
+            // frm_KPI_RPT_P
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -74,10 +75,9 @@ namespace Final
             this.Controls.Add(this.label2);
             this.Controls.Add(this.dgv_Process);
             this.Controls.Add(this.label1);
-            this.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.Name = "frm_KPI_RPT_1";
-            this.Text = "공정찾기";
-            this.Load += new System.EventHandler(this.frm_KPI_RPT_1_Load);
+            this.Name = "frm_KPI_RPT_P";
+            this.Text = "frm_KPI_RPT_P";
+            this.Load += new System.EventHandler(this.frm_KPI_RPT_P_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgv_Process)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -86,8 +86,8 @@ namespace Final
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DataGridView dgv_Process;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dgv_Process;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Final/Service/WorkCenterService.cs
+++ Final/Service/WorkCenterService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FinalDAC;
+using FinalVO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,12 @@ using System.Threading.Tasks;
 
 namespace Final.Service
 {
-    class WorkCenterService
+    public class WorkCenterService
     {
+        SelectDAC dac;
+        public List<WorkCenterVO> SelectWorkCenter()
+        {
+            return dac.SelectWorkCenter();
+        }
     }
 }
