b346493f06b0b798c839642f22951eddc668cdea
민주 <mj09287@gmail.com>
2021-02-10 01:20:23 +0900

    123

---
+++ FinalVO/LoginInfoVO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalVO
+{
+    static public class LoginInfoVO
+    {
+        static public string User_ID { get; set; }
+        static public string User_Name { get; set; }
+    }
+}
