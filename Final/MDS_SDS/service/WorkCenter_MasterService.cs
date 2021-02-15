1090fe32ce0153e09827fdeef31c5601a0654fc1
진성 이 <lee6996@nate.com>
2021-02-15 13:50:57 +0900

    ㄷㄷㄷ

---
+++ Final/MDS_SDS/service/WorkCenter_MasterService.cs
@@ -0,0 +1,29 @@
+﻿using FinalDAC;
+using FinalVO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final.MDS_SDS.service
+{
+    public class WorkCenter_MasterService
+    {       
+        public DataTable WorkCenterBinding()
+        {
+            return new WorkCenter_MasterDAC().WorkCenterBinding();
+        }
+
+        public List<WorkCenterVO> WorkCenterSelect(string code)
+        {
+            return new WorkCenter_MasterDAC().WorkCenterSelect(code);
+        }
+
+        public List<WorkCenterVO> GetAllWorkCenter()
+        {
+            return new WorkCenter_MasterDAC().GetAllWorkCenter();
+        }
+    }
+}
