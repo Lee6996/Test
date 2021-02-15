05a20bd980a87ab32707209d6a3be340388797b3
진성 이 <lee6996@nate.com>
2021-02-15 14:02:15 +0900

    11

--- FinalVO/WorkCenterVO.cs
+++ FinalVO/WorkCenterVO.cs
@@ -59,7 +59,26 @@ namespace FinalVO
         public string Up_Date { get; set; }
         // 최종수정자
         public string Up_Emp { get; set; }
-        
+
+
+    }
+    public class WorkCenter_Master2VO
+    {
+        public string Wc_Code { get; set; }                 // 작업장코드
+        public string Wc_Name { get; set; }                // 작업장명
+        public string Process_Code { get; set; }           // 공정코드
+        public string Process_name { get; set; } //공정이름
+        public string Wo_Status { get; set; }               // 비가동상태  
+        public string Last_Cnt_Time { get; set; }      // 최종실적처리시간
+        public string Prd_Req_Type { get; set; }           // 실적등록유형
+        public string Pallet_YN { get; set; }                 // 팔렛생성여부
+        public string Item_Code { get; set; }               // 최종작업품목
+        public string Prd_Unit { get; set; }                  // 실적단위
+        public string Mold_Setup_YN { get; set; }        // 금형장착여부
+        public string Use_YN { get; set; }                   // 사용여부
+        public string Remark { get; set; }                   // 비고
+
+        public string Ins_Emp { get; set; }
 
     }
 }
